d7734d60a22bcb122fb22c223f9f24d1f27be9f8
YanaLiso <114093396+YanaLiso@users.noreply.github.com>
2022-09-21 18:49:15 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+	/*int a;
+	cin >> a;
+	bool res = a > 0;
+	cout << boolalpha << res << endl;*/
+
+
+	/*int a;
+	cin >> a;
+	bool res = a % 2 != 0;
+	cout << boolalpha << res << endl;*/
+
+
+	/*int a;
+	cin >> a;
+	bool res = a % 2 == 0;
+	cout << boolalpha << res << endl;*/
+
+	/*int a, b;
+	cin >> a >> b;
+	bool res = a > 2 && b <= 3;
+	cout << boolalpha << res << endl;*/
+
+	/*int a, b;
+	cin >> a >> b;
+	bool res = a >= 0 || b < -2;
+	cout << boolalpha << res << endl;*/
+
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a < b && b < c;
+	cout << boolalpha << res << endl;*/
+
+
+
+
+
+	/* a, b, c;
+	cin >> a >> b >> c ;
+	bool res = (a < b && b<c||c > b && b > a);
+	cout << boolalpha << res << endl;*/
+
+/*nt a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 && b % 2 == 0;
+	cout << boolalpha << res << endl;*/
+
+/*int a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 || b% 2 == 0;
+	cout << boolalpha << res << endl;*/
+
+	int a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 && b % 2 != 0;
+	cout << boolalpha << res << endl;
+
+}
